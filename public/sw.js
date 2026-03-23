6b8288339550e6fff197aeee5e8a9b5f51743d96
Ignat <ignaturelli@gmail.com>
2026-03-23 13:15:58 -0300

    Initial commit: Ture 2026 biohacking tracker

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+// Service Worker for Ture 2026 - enables PWA install + notification support
+const CACHE_NAME = 'ture2026-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
